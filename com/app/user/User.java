8fa62b2908ae110b3d047a4e61f926072b1be8a5
sedhu.b <sedhu.b@zohocorp.com>
2025-03-22 22:11:43 +0530

    Added users.csv and User java class

---
+++ com/app/user/User.java
@@ -0,0 +1,13 @@
+package com.app.user;
+
+public class User {
+    public String name;
+    public Integer age;
+    public String gender;
+
+    public User(String name, Integer age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
